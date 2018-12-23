73c948df8ea7fb73a82cee0f8452a70793051793
AttilaMüller <muller.attila.aron@gmail.com>
2018-12-23 14:21:15 +0100

    Add html, stlye, and basic server.

--- basics/chat-baics.js
+++ basics/chat-baics.js
@@ -7,15 +7,17 @@ app.use(express.static(__dirname + '/public'));
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 
+// fired upon a connection from client -> can be used with namespace -> (same as connect)
+// without namespace we check for the connections at the main namespace
 io.on('connection', socket => {
+    // we emit an event to the socket identified by string name
     socket.emit('messageFromServer', { data: 'Welcome to the server' });
+    // we register a new handler for a given event
     socket.on('dataToServer', dataFromClient => {
         console.log(dataFromClient);
     });
-    socket.on('newMessageToServer', msg => {
-        io.emit('messageToClients', {
-            id: msg.id,
-            text: msg.text
-        });
+    // we check the reason of disconnect
+    socket.on('disconnect', reason => {
+        console.log(reason);
     });
 });
